167978bf896e66bea4d38df9a6bbc487f37c7f52
Pouapheng Yang <phewy06@yahoo.com>
2021-07-01 18:31:10 -0500

    delete added

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
@@ -14,5 +15,23 @@ const koalaRouter = express.Router();
 
 
 // DELETE
+Router.delete('/:id' (req, res) => {
+    console.log('Request URL: ', req.url);
+    console.log('Request route parameters: ', req.params);
+    const koalaId = req.params.id;
+    console.log(`koala id is ${koalaId}`);
+    
+    const qText = `DELETE FROM "koalas" WHERE id = $1;
+    `;
 
+    pool.query(qText, [koalaId])
+        .then(dbResponse => {
+            console.log(`${drResponse.rowCount === 1} was deleted from database`);
+            res.sendStatus(201)
+        })
+        .catch(error => {
+            console.log(`Could not delete koala with id ${koalaId}.`, error);
+            res.sendStatus(500);
+        });
+});
 module.exports = koalaRouter;
