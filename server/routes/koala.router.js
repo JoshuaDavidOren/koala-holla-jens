f90e321f29c59453af24adb0541ad783c15579a2
Joshua Oren <joshuadavidoren@gmail.com>
2021-07-01 16:43:53 -0500

    DB connection

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,7 +2,25 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
+const pg = require('pg');
+const Pool = pg.Pool;
+const config = {
+    database: 'save_koala',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000,
+};
 
+const pool = new Pool(config);
+
+pool.on('connect', (client) => {
+    console.log('PostgeSQL connected');
+});
+
+pool.on('error', (err, client) => {
+    console.log('Unexpected error client are you there?', err);
+});
 
 // GET
 
